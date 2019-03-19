35a161eee45a6e8503c6704687fcc2e5a0e0ee8f
sks3a09 <sks3a09@gmail.com>
2019-03-19 12:57:42 +0800

    how to pass and return parameter

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
+int sum(int a,int b){
+	int c = a+b;
+	return c;
+}
+int arithmetic(int a,int b,int c,int d,int e){
+	int f = (a+b)+(c*d)-e;
+	return f;
+}
+int pointer(int *a,int *b,int *c){
+	int d = *a + *b + *c;
+	return d;
+}
 void reset_handler(void)
 {
+	int a = 100;
+	int b = 200;
+	int c = 300;
+	sum(10,20);
+	arithmetic(1,2,3,4,5);
+	pointer(&a,&b,&c);
 	while (1)
 		;
 }
